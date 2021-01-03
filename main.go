40990f63a8fdd46f85c36c246475ee7cf1d99b33
andiksetyawan <andik.setyawan19@gmail.com>
2021-01-03 23:26:15 +0700

    main route

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"flag"
+	"github.com/andiksetyawan/rest-api-boilerplate-go/controller"
+	_ "github.com/andiksetyawan/rest-api-boilerplate-go/db"
+	"github.com/andiksetyawan/rest-api-boilerplate-go/middleware"
+	_ "github.com/andiksetyawan/rest-api-boilerplate-go/storage"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	bindAddr := flag.String("bind", ":8080", "bind addr")
+	flag.Parse()
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.POST("/login", controller.Login)
+	r.POST("/signup", controller.SignUp)
+
+	//r.Use(midlewares.IsAuth())
+	r.GET("/user", middleware.IsAuth(), controller.GetUser)
+	r.Run(*bindAddr)
+}
